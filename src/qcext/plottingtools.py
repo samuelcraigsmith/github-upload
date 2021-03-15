1f579787517bc8dbc3bbebec2c4573cdc4aa6b2f
samuelcraigsmith <ssmi7936@uni.sydney.edu.au>
2021-03-15 23:20:42 +1100

    Update plottingtools with an idea on merge

--- src/qcext/plottingtools.py
+++ src/qcext/plottingtools.py
@@ -5,7 +5,8 @@ import json
 
 
 def read_data(path):
-    """Return data from all .json files in path as a list of data points.
+    """
+    Return data from all .json files in path as a list of data points.
 
     Haven't checked that this actually works...
     """
@@ -26,28 +27,37 @@ def read_data(path):
 
     return data_set
 
-def extract_threshold_data(data_set, c=0.01):
-    """Data set are a list of python dictionaries.
 
-      Notes:
-        - The error bars are attained by treating the fail rate as a normally distributed
-    random variable. This breaks down as f goes to zero. For all f<c, we take f=c to
-    calculate generous error bars. For n=500 runs for each data point, c=0.01 is appropriate.
+def extract_threshold_data(data_set, c=0.01):
+    """
+    Extract thresholds from a list of data points.
+
+    Data set are a list of python dictionaries. The error bars are attained by
+    treating the fail rate as a normally distributed random variable. This
+    breaks down as f goes to zero. For all f<c, we take f=c to calculate
+    generous error bars. For n=500 runs for each data point, c=0.01 is
+    appropriate.
     """
     threshold_data = {}
+    unique_data_points = set()
     for data_point in data_set:
         data_point = data_point[0]  # qecsim returns a dict in a list.
         d = str(data_point["n_k_d"][2])
         if d not in threshold_data.keys():
             threshold_data[d] = [[], [], []]
-
         p = data_point["error_probability"]
-        f = data_point["n_fail"]/data_point["n_run"]
-        df = math.sqrt(max(f, c)*(1-max(f, c))/(data_point["n_run"]))
 
-        threshold_data[d][0].append(p)
-        threshold_data[d][1].append(f)
-        threshold_data[d][2].append(df)
+        if (d, p) not in unique_data_points:
+            f = data_point["n_fail"] / data_point["n_run"]
+            df = math.sqrt(max(f, c) * (1 - max(f, c)) / (data_point["n_run"]))
+
+            threshold_data[d][0].append(p)
+            threshold_data[d][1].append(f)
+            threshold_data[d][2].append(df)
+
+        else:
+            # need to find the entry corresponding to (d, p). This is not fast.
+            pass
     return threshold_data
 
 # def threshold_plot(data_set):
