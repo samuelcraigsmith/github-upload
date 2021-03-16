2e881b06d4aedfe0bb62b75149b5d0682c8f53ba
samuelcraigsmith <ssmi7936@uni.sydney.edu.au>
2021-03-16 13:03:39 +1100

    Update extract_threshold_data and read_data
    Update read_data to ignore hidden files created by text editors on mac (._[name]) files. Fix some bugs in extract_threshold_data. Works on x_data="time_steps".

--- src/qcext/plottingtools.py
+++ src/qcext/plottingtools.py
@@ -1,4 +1,4 @@
-d"""Tools for extracting data from qecsim and qcext for plotting."""
+"""Tools for extracting data from qecsim and qcext for plotting."""
 import math
 import os
 import json
@@ -10,11 +10,14 @@ def read_data(path):
 
     Haven't checked that this actually works...
     """
+
     cwd_init = os.getcwd()  # will return to cwd to avoid side effects.
     os.chdir(path)
 
-    data_files = [data_file for data_file in os.listdir() if
-                  os.path.splitext(data_file)[-1] == ".json"]
+    data_files = [data_file for data_file in os.listdir()
+                  if os.path.splitext(data_file)[-1] == ".json"
+                  and data_file[0] != "."]  # ignore hidden files
+
     data_set = []
     for data_file in data_files:
         with open(data_file, "r") as f:
@@ -46,7 +49,7 @@ def extract_threshold_data(data_set, x_data=None, x_data_label=None, c=0.01):
     # defaults
     x_data = "error_probability" if x_data is None else x_data
     if x_data_label is None:
-        x_data_label = x_data if x_data is str else ""
+        x_data_label = x_data if type(x_data) is str else ""
 
     threshold_data = {}
     for i, data_point in enumerate(data_set):
@@ -56,7 +59,7 @@ def extract_threshold_data(data_set, x_data=None, x_data_label=None, c=0.01):
             threshold_data[d] = {x_data_label: [], "f": [], "df": []}
 
         # index into x_data instead of zip due to conditional nature of x_data.
-        x = data_point[x_data] if x_data is str else x_data[i]
+        x = data_point[x_data] if type(x_data) is str else x_data[i]
 
         f = data_point["n_fail"] / data_point["n_run"]
         df = math.sqrt(max(f, c) * (1 - max(f, c)) / (data_point["n_run"]))
@@ -65,7 +68,7 @@ def extract_threshold_data(data_set, x_data=None, x_data_label=None, c=0.01):
         threshold_data[d]["f"].append(f)
         threshold_data[d]["df"].append(df)
 
-    return threshold_data
+    return threshold_datax
 
 # def threshold_plot(data_set):
 #     fig, ax = plt.subplots()
