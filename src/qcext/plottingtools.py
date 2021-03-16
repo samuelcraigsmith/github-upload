d6e8130ce13d29369733c48faf61e07fce4695e9
samuelcraigsmith <ssmi7936@uni.sydney.edu.au>
2021-03-16 11:12:04 +1100

    Update plottingtools
    Add functionality to allow user to specify x_data series or to provide their own. Not tested.

--- src/qcext/plottingtools.py
+++ src/qcext/plottingtools.py
@@ -37,6 +37,10 @@ def extract_threshold_data(data_set, x_data=None, x_data_label=None, c=0.01):
     breaks down as f goes to zero. For all f<c, we take f=c to calculate
     generous error bars. For n=500 runs for each data point, c=0.01 is
     appropriate.
+
+    Where x_data is in "error_probability", "time_steps", np.ndarray (custom)
+    and defaults to "error_probability". x_data_label defaults to x_data if
+    x_data is a string or the empty string.
     """
 
     # defaults
@@ -45,19 +49,21 @@ def extract_threshold_data(data_set, x_data=None, x_data_label=None, c=0.01):
         x_data_label = x_data if x_data is str else ""
 
     threshold_data = {}
-    for data_point in data_set:
+    for i, data_point in enumerate(data_set):
         data_point = data_point[0]  # qecsim returns a dict in a list.
         d = str(data_point["n_k_d"][2])
         if d not in threshold_data.keys():
-            threshold_data[d] = [[], [], []]
-        p = data_point["error_probability"]
+            threshold_data[d] = {x_data_label: [], "f": [], "df": []}
+
+        # index into x_data instead of zip due to conditional nature of x_data.
+        x = data_point[x_data] if x_data is str else x_data[i]
 
         f = data_point["n_fail"] / data_point["n_run"]
         df = math.sqrt(max(f, c) * (1 - max(f, c)) / (data_point["n_run"]))
 
-        threshold_data[d][0].append(p)
-        threshold_data[d][1].append(f)
-        threshold_data[d][2].append(df)
+        threshold_data[d][x_data_label].append(x)
+        threshold_data[d]["f"].append(f)
+        threshold_data[d]["df"].append(df)
 
     return threshold_data
 
